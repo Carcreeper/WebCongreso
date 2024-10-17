1c563b2bf4881e3c64b008ae81a2c5b884869801
Darwilton-300 <darwilton.herrera.perez@gmail.com>
2024-10-17 14:01:14 -0500

    version1.3

--- readcharla.php
+++ readcharla.php
@@ -6,12 +6,22 @@
 
   $con = mysqli_connect($servidor, $usuario, $pass, $database);
 
-  $sql = "SELECT * FROM `charlas` WHERE"
+  $sql = "SELECT * FROM `charlas` WHERE";
   $resp = $con->query($sql); 
 
   if ($resp -> num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - nombrecharla: " . $row["nombrecharla"]. " "  "<br>";
+      ?>
+       <div class="card">
+                <div>
+                  <h1><?php echo "id: " . $row["id"]. " - nombrecharla: " . $row["nombrecharla"]. " " . "<br>";?></h1>           
+                </div>
+            <div class="card__content">
+              <a href="" class="button v2">ver comentarios</a>
+            </div>
+
+        
+      <?php
       }
     }
   else{
