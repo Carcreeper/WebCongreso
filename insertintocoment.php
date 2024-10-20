07cf2f684d1fffb668313b23a3a0fc890e8439f7
Darwilton-300 <darwilton.herrera.perez@gmail.com>
2024-10-20 17:26:27 -0500

    version 2.1

--- insertintocoment.php
+++ insertintocoment.php
@@ -1,4 +1,5 @@
 <?php
+    $id_charla      = $_POST['id_charla'];
     $usuario2       = $_POST['usuario'];
     $comentario     = $_POST['comentario'];
     $fechayhora     = $_POST['fechayhora'];
@@ -10,7 +11,7 @@
 
     $con = mysqli_connect($servidor, $usuario, $pass, $database);
 
-    $sql = "INSERT INTO comentarios (usuario, comentario, fechayhora) VALUES ('$usuario2', '$comentario', '$fechayhora')";
+    $sql = "INSERT INTO comentarios (id_charla, usuario, comentario, fechayhora) VALUES ('$id_charla ', '$usuario2', '$comentario', '$fechayhora')";
     $con->query($sql);
 
     $con->close();
