2d63756a03d4842427ff9c4514e629301c9b872b
Darwilton-300 <darwilton.herrera.perez@gmail.com>
2024-10-20 16:15:34 -0500

    version2.0

---
+++ coments.php
@@ -0,0 +1,37 @@
+<?php
+  $servidor = 'localhost';
+  $database = 'app_congreso';
+  $usuario  = 'root';
+  $pass	  = '';
+
+  $id = $_GET ["id"];
+
+  $con = mysqli_connect($servidor, $usuario, $pass, $database);
+
+  $sql = "SELECT * FROM `comentarios` WHERE id_charla =  ". $id;
+  $resp = $con->query($sql); 
+
+  if ($resp->num_rows > 0) {
+    while($row = $resp->fetch_assoc()) {
+        ?>
+            <div>
+                <p><?php echo $row["usuario"]?>     </p>
+                <p><?php echo $row["comentario"]?>  </p>
+                <p><?php echo $row["fechayhora"]?>  </p>
+            </div>    
+      <?php
+      }
+    }
+  else{
+        echo "no hay charlas";
+    }
+
+
+
+    
+
+
+
+
+
+?>
--- readcharla.php
+++ readcharla.php
@@ -6,21 +6,19 @@
 
   $con = mysqli_connect($servidor, $usuario, $pass, $database);
 
-  $sql = "SELECT * FROM `charlas` WHERE";
+  $sql = "SELECT * FROM `charlas`";
   $resp = $con->query($sql); 
 
-  if ($resp -> num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
+  if ($resp->num_rows > 0) {
+    while($row = $resp->fetch_assoc()) {
       ?>
        <div class="card">
                 <div>
-                  <h1><?php echo "id: " . $row["id"]. " - nombrecharla: " . $row["nombrecharla"]. " " . "<br>";?></h1>           
+                  <h1><?php echo "- nombrecharla: " . $row["nombrecharla"]. " " . "<br>";?></h1>           
                 </div>
             <div class="card__content">
-              <a href="" class="button v2">ver comentarios</a>
+              <a href="coments.php?id=<?php echo $row["id"] ?>" class="button v2">ver comentarios</a>
             </div>
-
-        
       <?php
       }
     }
