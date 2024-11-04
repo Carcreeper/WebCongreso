e0cd4fafde43e79c1aca09954e0ca4c76bf3d451
calxifer <lu9smateo@gmail.com>
2024-11-04 18:20:58 -0500

    coments
    estilo de coments - sin responsive

--- coments.php
+++ coments.php
@@ -1,28 +1,46 @@
-<?php
-  $servidor = 'localhost';
-  $database = 'app_congreso';
-  $usuario  = 'root';
-  $pass	  = '';
-
-  $id = $_GET ["id"];
-
-  $con = mysqli_connect($servidor, $usuario, $pass, $database);
-
-  $sql = "SELECT * FROM `comentarios` WHERE id_charla =  ". $id;
-  $resp = $con->query($sql); 
-
-  if ($resp->num_rows > 0) {
-    while($row = $resp->fetch_assoc()) {
-        ?>
-            <div>
-                <p><?php echo $row["usuario"]?>     </p>
-                <p><?php echo $row["comentario"]?>  </p>
-                <p><?php echo $row["fechayhora"]?>  </p>
-            </div>    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="comentsStyle.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="CartWhit">
+  <div class="CartBlueImg">
+     <div class="CartBlue">
       <?php
-      }
-    }
-  else{
-        echo "no hay charlas";
-    }
-?>
+          $servidor = 'localhost';
+          $database = 'app_congreso';
+          $usuario  = 'root';
+          $pass	  = '';
+
+          $id = $_GET ["id"];
+
+          $con = mysqli_connect($servidor, $usuario, $pass, $database);
+
+          $sql = "SELECT * FROM `comentarios` WHERE id_charla =  ". $id;
+          $resp = $con->query($sql); 
+
+          if ($resp->num_rows > 0) {
+            while($row = $resp->fetch_assoc()) {
+                ?>
+                    <div>
+                        <p><?php echo $row["usuario"]?>     </p>
+                        <p><?php echo $row["comentario"]?>  </p>
+                        <p><?php echo $row["fechayhora"]?>  </p>
+                    </div>    
+              <?php
+              }
+            }
+          else{
+            echo "no hay charlas";
+            }
+        ?>
+      </div>
+  </div>
+</div>
+</body>
+</html>
--- readcharla.php
+++ readcharla.php
@@ -3,52 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Productos</title>
+    <title>redcharla</title>
     <link rel="stylesheet" href="RedCharlaStyle.css">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <header>
-    </header>
-   
-    <div class="container">
+<div class="container">
 
-        <div class="CartWhit">
-        <div class="CartBlueImg">
-        <div class="CartBlue">
-            <?php
-  $servidor = 'localhost';
-  $database = 'app_congreso';
-  $usuario  = 'root';
-  $pass	  = '';
+  <div class="CartWhit">
+    <div class="CartBlueImg">
+      <div class="CartBlue">
+         <?php
+            $servidor = 'localhost';
+            $database = 'app_congreso';
+            $usuario  = 'root';
+            $pass	  = '';
 
-  $con = mysqli_connect($servidor, $usuario, $pass, $database);
+            $con = mysqli_connect($servidor, $usuario, $pass, $database);
 
-  $sql = "SELECT * FROM `charlas`";
-  $resp = $con->query($sql); 
+            $sql = "SELECT * FROM `charlas`";
+            $resp = $con->query($sql); 
 
-  if ($resp->num_rows > 0) {
-    while($row = $resp->fetch_assoc()) {
-      ?>
-       <div class="card">
-                <div>
-                  <h1><?php echo "- nombrecharla: " . $row["nombrecharla"]. " " . "<br>";?></h1>           
-                </div>
-            <div class="card__content">
-              <a href="coments.php?id=<?php echo $row["id"] ?>" class="button v2">ver comentarios</a>
-            </div>
-        </div>
-      <?php
-      }
-    }
-  else{
-        echo "no hay charlas";
-    }
+            if ($resp->num_rows > 0) {
+              while($row = $resp->fetch_assoc()) {
+                ?>
+                <div class="card">
+                          <div>
+                            <h1><?php echo "- nombrecharla: " . $row["nombrecharla"]. " " . "<br>";?></h1>           
+                          </div>
+                      <div class="card__content">
+                        <a href="coments.php?id=<?php echo $row["id"] ?>" class="button v2">ver comentarios</a>
+                      </div>
+                  </div>
+                <?php
+                }
+              }
+            else{
+                  echo "no hay charlas";
+              }
 
-    $con->close();
-?>
-    
-           
+              $con->close();
+          ?>
+              
+                    
           </div>
         </div>
     </div>      
